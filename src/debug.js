a62b0866d095d58eaaa1cb08a85659b7c9783907
embryobirthday <alexinverda@gmail.com>
2024-04-08 14:22:20 -0400

    completed coding parts :]

--- src/debug.js
+++ src/debug.js
@@ -1,20 +1,23 @@
 const brokenLoop = (start, end) => {
   let result = '';
-  for (let i = start; i < end; i += 2) {
+  for (let i = start; i <= end; i++) { //inclusive meaning that it's not whether i is less than the end, but less than or equal to. which includes the end number.
     result += i;
   }
   return result;
 };
 
+//need someone to explain this to me
 const brokenNested = () => {
   let result = '';
   for (let i = 0; i < 2; i++) {
-    for (let i = 0; i < 10; i++) {
-      result += `-${i}${i}`;
+    for (let j = 0; j < 10; j++) { //not entirely sure i understand the order of printing this, but the function was re-assigning i by having i represent an index in both for loops.
+      result += `-${i}${j}`;
     }
   }
   return result.slice(1);
 };
+
+console.log(brokenNested());
 
 module.exports = {
   brokenLoop,
--- src/from-scratch.js
+++ src/from-scratch.js
@@ -1,21 +1,52 @@
 const loop0UpTo10 = () => {
+  for (let i = 0; i < 10; i++){
+    console.log(i)
+  }
 };
 
 const loop5to10 = () => {
+  for (let i = 5; i <= 10; i++){
+    console.log(i)
+  }
 };
 
 // you do NOT need an if check
 const loopEvenNumbersUpTo10 = () => {
+  for (let i = 0; i < 10; i += 2) //setting the base for counting from zero, then setting the condition that
+    console.log(i) // ^^ ...as long as i is less than 10, count up by adding two. this logs all the even numbers within 10.
 };
+// console.log("loopEvenNumbersUpTo10 test")
+// loopEvenNumbersUpTo10(); //test
 
 const countdown5to0 = () => {
+  for (let i = 5; i >= 0; i--){ //starting point is 5, as long as i is more than or equal to 0, count down (i--)
+    console.log(i)
+  }
 };
+// console.log("countdown5to0 test")
+// countdown5to0(); //test
 
-const loopUpToNum = () => {
+const loopUpToNum = (num) => {
+  for (i = 0; i < num; i++){
+    console.log(i)
+  }
 };
 
 const fizzbuzz = () => {
+  for (i = 1; i <= 100; i++) {
+    if (i % 3 === 0 && i % 5 === 0){
+      console.log("fizzbuzz")}
+      else if (i % 3 === 0){
+        console.log("fizz")
+      } else if (i % 5 === 0) {
+        console.log("buzz")
+      } else {
+        console.log(i)
+      }
+  } 
 };
+
+console.log(fizzbuzz());
 
 module.exports = {
   loop0UpTo10,
